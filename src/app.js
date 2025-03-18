ce506a5a9e29448f2d74a9e092d67045a9ed1628
Benny Oliveira <bennycarvalho13@gmail.com>
2025-03-18 01:01:42 +0000

    Added random colors to Excuse Generator

--- src/app.js
+++ src/app.js
@@ -16,9 +16,12 @@ window.onload = function() {
   let excuse = generateExcuse(who, action, what, when);
   let excuseElement = document.getElementById("excuse");
   excuseElement.innerHTML = excuse;
+  randomColor();
 
-  document.getElementById("button").addEventListener("click", newExcuse);
+  document.getElementById("exButton").addEventListener("click", newExcuse);
+  document.getElementById("exButton").addEventListener("click", randomColor);
   document.getElementById("hideExBtn").addEventListener("click", hideExcuseDiv);
+  document.getElementById("excuse").addEventListener("click", copyToClip);
 };
 
 function newExcuse() {
@@ -47,14 +50,45 @@ function generateExcuse(array1, array2, array3, array4) {
   );
 }
 
+function randomColor() {
+  let excuseBack = document.getElementById("excuseGen");
+  let excuseText = document.getElementById("excuse");
+  let excuseBtn = document.getElementById("exButton");
+
+  let textRed = Math.floor(Math.random() * 256);
+  let textGreen = Math.floor(Math.random() * 256);
+  let textBlue = Math.floor(Math.random() * 256);
+
+  let btnRed = Math.floor(Math.random() * 256);
+  let btnGreen = Math.floor(Math.random() * 256);
+  let btnBlue = Math.floor(Math.random() * 256);
+
+  let textLuminance = 0.2126 * textRed + 0.7152 * textGreen + 0.0722 * textBlue;
+  let btnLuminance = 0.2126 * btnRed + 0.7152 * btnGreen + 0.0722 * btnBlue;
+
+  excuseText.style.color = textLuminance < 110 ? "white" : "black";
+  excuseBtn.style.color = btnLuminance < 110 ? "white" : "black";
+
+  excuseBack.style.backgroundColor = `rgb(${textRed}, ${textGreen}, ${textBlue})`;
+  excuseBtn.style.backgroundColor = `rgb(${btnRed}, ${btnGreen}, ${btnBlue})`;
+}
+
 function hideExcuseDiv() {
-  var excuseDiv = document.getElementById("excuseGen");
+  let excuseDiv = document.getElementById("excuseGen");
   let button = document.getElementById("hideExBtn");
-  if (excuseDiv.style.display === "none") {
-    excuseDiv.style.display = "block";
+
+  if (excuseDiv.classList.contains("hidden")) {
+    excuseDiv.classList.remove("hidden");
     button.innerHTML = '<i class="fa-solid fa-eye-slash"></i>';
   } else {
-    excuseDiv.style.display = "none";
+    excuseDiv.classList.add("hidden");
     button.innerHTML = '<i class="fa-solid fa-eye"></i>';
   }
+}
+
+function copyToClip() {
+  navigator.clipboard.writeText(document.getElementById("excuse").textContent);
+
+  // Alert the copied text
+  alert("Copied to Cilpboard");
 }
